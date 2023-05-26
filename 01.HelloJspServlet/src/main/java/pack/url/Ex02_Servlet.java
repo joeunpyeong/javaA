335a8073f6d8998c5a3f383c4b77fd52a86b779e
joeunpyeong <128439456+joeunpyeong@users.noreply.github.com>
2023-05-26 17:54:29 +0900

    z

---
+++ 01.HelloJspServlet/src/main/java/pack/url/Ex02_Servlet.java
@@ -0,0 +1,20 @@
+package pack.url;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Ex02_Servlet
+ */
+@WebServlet("/Ex02_Servlet")
+public class Ex02_Servlet extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Ex02_Servlet");
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+}
